a9a2d5aa1d4e64b115359b3bff978b187d387e9b
Jason O'Neil <jason.oneil@gmail.com>
2016-12-24 15:48:13 +0800

    Refactor lineChart.js
    - Separate things out into smaller functions
    - Rename lineGraph / lineChart
    - Prepare for supporting multiple lines

--- lineChart.js
+++ lineChart.js
@@ -1,84 +1,104 @@
 (function () {
 	"use strict";
-	var LineGraph = function(authorData, deps) {
+
+	function addStylesheet (lineChart, container) {
+		var lineColor = lineChart.authorData.lineColor || 'steelblue',
+			stylesheetElm = document.createElement("style"),
+			rule = ".line { fill: none; stroke: " + lineColor + "; stroke-width: 1.5px; }";
+		container.appendChild(stylesheetElm);
+		stylesheetElm.sheet.insertRule(rule, 0);
+	}
+
+	function drawGraphCanvas(lineChart, container) {
+		var d3 = lineChart.d3,
+			cont = d3.select(container),
+			canvasWidth = 960,
+			canvasHeight = 500,
+			svg = cont.append("svg")
+				.attr("width", canvasWidth)
+				.attr("height", canvasHeight),
+			margin = {top: 20, right: 20, bottom: 30, left: 50},
+			width = canvasWidth - margin.left - margin.right,
+			height = canvasHeight - margin.top - margin.bottom,
+			graph = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		return {
+			graph: graph,
+			width: width,
+			height: height
+		};
+	}
+
+	function loadTsvAndDrawLines(lineChart, canvas) {
+		var d3 = lineChart.d3,
+			props = lineChart.authorData,
+			tsvUrl = (props.tsv.indexOf('://') > -1) ? props.tsv : lineChart.dataPath + props.tsv,
+			dateFormat = props.dateFormat,
+			xField = props.xField,
+			yField = props.yField,
+			yLabel = props.yLabel,
+			parseTime = d3.timeParse(dateFormat),
+			x = d3.scaleTime()
+				.rangeRound([0, canvas.width]),
+			y = d3.scaleLinear()
+				.rangeRound([canvas.height, 0]);
+
+		var line = d3.line()
+			.x(function(d) { return x(d[xField]); })
+			.y(function(d) { return y(d[yField]); });
+
+		d3.tsv(tsvUrl, function(d) {
+			// Map the xField to be a real date object, not a date string.
+			d[xField] = parseTime(d[xField]);
+			d[yField] = parseInt(d[yField], 10);
+			return d;
+		}, function(error, data) {
+			if (error) throw error;
+
+			x.domain(d3.extent(data, function(d) { return d[xField]; }));
+			y.domain(d3.extent(data, function(d) { return d[yField]; }));
+
+			canvas.graph.append("g")
+				.attr("class", "axis axis--x")
+				.attr("transform", "translate(0," + canvas.height + ")")
+				.call(d3.axisBottom(x));
+
+			canvas.graph.append("g")
+				.attr("class", "axis axis--y")
+				.call(d3.axisLeft(y))
+				.append("text")
+				.attr("fill", "#000")
+				.attr("transform", "rotate(-90)")
+				.attr("y", 6)
+				.attr("dy", "0.71em")
+				.style("text-anchor", "end")
+				.text(yLabel);
+
+			canvas.graph.append("path")
+				.datum(data)
+				.attr("class", "line")
+				.attr("d", line);
+		});
+	};
+
+	var LineChart = function(authorData, deps) {
 		this.authorData = authorData;
 		this.dataPath = authorData.enthral.dataPath;
 		this.d3 = deps.d3;
 	};
-	LineGraph.prototype = {
+
+	LineChart.prototype = {
 		setupView: function(container) {
-			var d3 = this.d3,
-				tsvFile = this.authorData.tsv,
-				tsvUrl = (tsvFile.indexOf('://') > -1) ? tsvFile : this.dataPath + tsvFile,
-				dateFormat = this.authorData.dateFormat,
-				xField = this.authorData.xField,
-				yField = this.authorData.yField,
-				yLabel = this.authorData.yLabel,
-				lineColor = this.authorData.lineColor || 'steelblue',
-				cont = d3.select(container),
-				svg = cont.append("svg")
-					.attr("width", "960")
-					.attr("height", "500"),
-				margin = {top: 20, right: 20, bottom: 30, left: 50},
-				width = +svg.attr("width") - margin.left - margin.right,
-				height = +svg.attr("height") - margin.top - margin.bottom,
-				g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-			var parseTime = d3.timeParse(dateFormat);
-
-			var stylesheetElm = document.createElement("style");
-			document.head.appendChild(stylesheetElm);
-			var sheet = stylesheetElm.sheet,
-				rule = ".line { fill: none; stroke: " + lineColor + "; stroke-width: 1.5px; }";
-			sheet.insertRule(rule, sheet.cssRules.length);
-
-			var x = d3.scaleTime()
-				.rangeRound([0, width]);
-
-			var y = d3.scaleLinear()
-				.rangeRound([height, 0]);
-
-			var line = d3.line()
-				.x(function(d) { return x(d[xField]); })
-				.y(function(d) { return y(d[yField]); });
-
-			d3.tsv(tsvUrl, function(d) {
-				d[xField] = parseTime(d[xField]);
-				d[yField] = +d[yField];
-				return d;
-			}, function(error, data) {
-				if (error) throw error;
-
-				x.domain(d3.extent(data, function(d) { return d[xField]; }));
-				y.domain(d3.extent(data, function(d) { return d[yField]; }));
-
-				g.append("g")
-					.attr("class", "axis axis--x")
-					.attr("transform", "translate(0," + height + ")")
-					.call(d3.axisBottom(x));
-
-				g.append("g")
-					.attr("class", "axis axis--y")
-					.call(d3.axisLeft(y))
-					.append("text")
-					.attr("fill", "#000")
-					.attr("transform", "rotate(-90)")
-					.attr("y", 6)
-					.attr("dy", "0.71em")
-					.style("text-anchor", "end")
-					.text(yLabel);
-
-				g.append("path")
-					.datum(data)
-					.attr("class", "line")
-					.attr("d", line);
-			});
-
+			addStylesheet(this, container);
+			var canvas = drawGraphCanvas(this, container);
+			loadTsvAndDrawLines(this, canvas);
 		}
 	};
-	LineGraph.enthralPropTypes = {};
-	LineGraph.enthralDependencies = {
+
+	LineChart.enthralPropTypes = {};
+
+	LineChart.enthralDependencies = {
 		'd3': 'https://d3js.org/d3.v4.min.js'
 	};
-	window.LineGraph = LineGraph;
+
+	window.LineChart = LineChart;
 })();
